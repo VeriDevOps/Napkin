81ce3b2ce0c073855823d70ec047f82077ce39e0
Daniel Flemström <daniel.flemstrom@mdh.se>
2021-01-14 13:14:33 +0100

    Cleaned up ace and brace

--- client/js/ace/build/src-min-noconflict/snippets/tears.js
+++
@@ -1,1 +0,0 @@
-ace.define("ace/snippets/tears",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="tears"})
--- client/js/ace/build/src-min/snippets/tears.js
+++
@@ -1,1 +0,0 @@
-define("ace/snippets/tears",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="tears"})
