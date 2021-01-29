fbb5296af14cffa6f1bba7107c8ff813e5876782
Daniel Flemström <daniel.flemstrom@ri.se>
2021-01-29 09:07:32 +0100

    Added start script for servers without npm run dev

---
+++ start_servers.sh
@@ -0,0 +1,6 @@
+
+# This script is used for starting the servers 
+# when the index.htm in dist is used instead of 
+# npm run dev 
+python3 server/server.py& 
+python3 packages_src/bt/bt/auto_updater.py&
